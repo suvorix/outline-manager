0a171631c0d95b15b5573b981756bc2168bcc8b0
Suvorov Nikita <34267845+suvorix@users.noreply.github.com>
2025-11-16 14:25:21 +0300

    #3 - Favicon + Yandex Tableau

--- views/templates/head.php
+++ views/templates/head.php
@@ -4,3 +4,13 @@
 <link rel="stylesheet" href="/assets/css/vars.css">
 <link rel="stylesheet" href="/assets/css/style.css">
 <script src="/assets/libs/jquery-3.7.0.min.js"></script>
+
+<!-- [START] FAVICON BLOCK -->
+<link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
+<link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Outline Manager" />
+<link rel="yandex-tableau-widget" href="/assets/favicon/yandex-tableau-widget.json">
+<link rel="manifest" href="/assets/favicon/site.webmanifest" />
+<!-- [END] FAVICON BLOCK -->
